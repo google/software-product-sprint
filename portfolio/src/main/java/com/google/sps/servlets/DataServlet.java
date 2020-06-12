d6c5ed4989cf375aaaa523d69e6a414c4039bfd1
Andrew <andrew.rubinstein93@gmail.com>
2020-06-12 19:30:02 -0400

    Add greeting for Andrew on Data path

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -27,6 +27,6 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Hello, Andrew!</h1>");
   }
 }
