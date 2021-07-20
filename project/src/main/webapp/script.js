7db7b0acd369eec9cd5b12647da8730dbdf1cd59
FerrinThreatt <fthreatt19@gmail.com>
2021-07-20 16:38:44 +0000

    Fetching a playlist

--- project/src/main/webapp/script.js
+++ project/src/main/webapp/script.js
@@ -32,6 +32,7 @@ function darkMode() {
 }
 
 const _getToken = async () => {
+    //OLD ID's
     const clientId = '65b8fa1fad84447391094d21e2907ffa';
     const clientSecret = '68540d65fa6a48ea8fc69d6096284459';
     
@@ -49,13 +50,18 @@ const _getToken = async () => {
     const data = await result.json();
     return data.access_token;
 }
-//playlist
 
-fetch('https://api.spotify.com/v1/playlist/3cEYpjA9oz9GiPac4AsH4n', {
+var bpmToPlaylistId ={'90': '5fQuCLAEcNtHbFTdyiEJDd', '128': '28p4zE67Svf2S1OIZlp0rh', '150': '37i9dQZF1DX0hWmn8d5pRe'};
+var wortoutNameToBpm = {'90': 'yoga', '128' : 'abs', '150':'running'};
+//workout based on user input?
+// var workoutType = window.prompt("Enter your workout Type: ");
+// alert("workout: " + workoutType);
+const accessToken = 'BQBE8LgaXhAp-aTLdd4cpLuTV85YBhqXVtXqFfg5HB69mIBfSXSBNEqdwjmLnhuiQHc07KbjbhV8tzQXaOEAEJGwdjVRoVyLUu-m8qbzX3d3iuyW-wE0dcoCIRBOqBFghev4cDFqjThqwv4R6A-rMNJ3';
+fetch('https://api.spotify.com/v1/playlists/37i9dQZF1DX0hWmn8d5pRe', {
     method: 'GET', headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + _getToken(),
+        'Authorization': 'Bearer ' + accessToken
     }
 })
     .then((response) => {
